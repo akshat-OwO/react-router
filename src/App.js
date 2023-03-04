0facfd4793160df443dc71c08700d10badf09352
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-03-04 22:23:03 +0530

    Nested Routes & Layouts

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ import {
 // pages
 import About from './pages/About';
 import Home from './pages/Home';
+import Faq from './pages/help/Faq';
+import Contact from './pages/help/Contact';
 
 // layout imports
 import RootLayout from './layouts/RootLayout';
+import HelpLayout from './layouts/HelpLayout';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
+            <Route path='help' element={<HelpLayout />}>
+                <Route path='faq' element={<Faq />} />
+                <Route path='contact' element={<Contact />} />
+            </Route>
         </Route>
     )
 );
---
+++ src/pages/help/Faq.js
@@ -0,0 +1,22 @@
+const Faq = () => {
+    return (
+        <div className="faq">
+            <h3>Frequently Asked Questions</h3>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptates repellat, expedita sed maxime ipsam odit dolores, illum illo consectetur itaque, repudiandae neque facilis maiores.</p>
+            </div>
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptates repellat, expedita sed maxime ipsam odit dolores, illum illo consectetur itaque, repudiandae neque facilis maiores.</p>
+            </div>
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptates repellat, expedita sed maxime ipsam odit dolores, illum illo consectetur itaque, repudiandae neque facilis maiores.</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Faq;
